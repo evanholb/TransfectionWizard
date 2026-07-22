cdb122aaebd443885ff709ca06ea994c96576b23
Evan Holbrook <evan.holbrook@icloud.com>
2026-07-22 16:13:21 -0400

    Add files via upload

--- core/state.py
+++ core/state.py
@@ -62,13 +62,12 @@ class TemplateState:
         active: Currently selected template name
     """
     built_in: Dict[str, str] = field(default_factory=lambda: {
-        'v3.8': 'data/OT2_automated_transfection_v3.8.py',
-        'v3.9': 'data/OT2_automated_transfection_v3.9.py',
-        '96-well': 'data/OT2_automated_transfection_test96well_format.py',
-        '96-well P3000 first': 'data/OT2_automated_transfection_test96well_format_P3000 first.py'
+        #'v3.8': 'data/OT2_automated_transfection_v3.8.py',
+        #'v3.9': 'data/OT2_automated_transfection_v3.9.py',
+        'v4.0': 'data/OT2_automated_transfection_v4.0.py'
     })
     custom: Dict[str, str] = field(default_factory=dict)
-    active: str = 'v3.9'
+    active: str = 'v4.0'
 
     def add_custom(self, filename: str, content: str):
         """Add a custom template and make it active."""
